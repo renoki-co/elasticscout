9d4c258b2680bac92217aa1780364d382ada72bf
Alex Renoki <rennokki@gmail.com>
2020-01-15 19:47:11 +0200

    wip query tests.

---
+++ tests/QueryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Rennokki\ElasticScout\Tests;
+
+use Rennokki\ElasticScout\Tests\Models\Book;
+use Rennokki\ElasticScout\Tests\Models\Post;
+use Rennokki\ElasticScout\Tests\Models\Restaurant;
+
+class QueryTest extends TestCase
+{
+    public function test_search_book_by_name()
+    {
+        $book = factory(Book::class)->make();
+        $book->getIndex()->sync();
+        $book->save();
+
+        $searchResult = Book::search($book->name)->first();
+
+        $this->assertTrue($searchResult->is($book));
+    }
+
+    public function test_search_book_by_partial_starting_name_leads_to_no_results()
+    {
+        $book = factory(Book::class)->make();
+        $book->getIndex()->sync();
+        $book->save();
+
+        $partialBookName = substr($book->name, 0, 5);
+
+        $this->assertNull(
+            Book::search($partialBookName)->first()
+        );
+    }
+
+    public function test_search_restaurant_by_partial_name_leads_to_results_due_to_ngram_analyzer()
+    {
+        $restaurant = factory(Restaurant::class)->make();
+        $restaurant->getIndex()->sync();
+        $restaurant->save();
+
+        $partialRestaurantName = substr($restaurant->name, 0, 5);
+
+        $searchResult = Restaurant::search($partialRestaurantName)->first();
+
+        $this->assertTrue($searchResult->is($restaurant));
+    }
+}
