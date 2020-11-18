dd222d62096b3aa10fcd505c783a6a8edb320c97
Alex Renoki <rennokki@gmail.com>
2020-11-18 18:54:23 +0200

    Updated confgi

--- config/elasticscout.php
+++ config/elasticscout.php
@@ -5,8 +5,13 @@ return [
     'connection' => [
 
         /*
-         * Define your Elasticsearch connection here.
-         */
+        |--------------------------------------------------------------------------
+        | Elasticscout Connections
+        |--------------------------------------------------------------------------
+        |
+        | Define your Elasticsearch connection here.
+        |
+        */
 
         'hosts' => [
             [
@@ -23,142 +28,178 @@ return [
             ],
         ],
 
-        /**
-         * SSL.
-         *
-         * If your Elasticsearch instance uses an out-dated or self-signed SSL
-         * certificate, you will need to pass in the certificate bundle.  This can
-         * either be the path to the certificate file (for self-signed certs), or a
-         * package like https://github.com/Kdyby/CurlCaBundle.  See the documentation
-         * below for all the details.
-         *
-         * If you are using SSL instances, and the certificates are up-to-date and
-         * signed by a public certificate authority, then you can leave this null and
-         * just use "https" in the host path(s) above and you should be fine.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_security.html#_ssl_encryption_2
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | SSL Verification
+        |--------------------------------------------------------------------------
+        |
+        | If your Elasticsearch instance uses an out-dated or self-signed SSL
+        | certificate, you will need to pass in the certificate bundle.  This can
+        | either be the path to the certificate file (for self-signed certs), or a
+        | package like https://github.com/Kdyby/CurlCaBundle.  See the documentation
+        | below for all the details.
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_security.html#_ssl_encryption_2
+        |
+        | If you are using SSL instances, and the certificates are up-to-date and
+        | signed by a public certificate authority, then you can leave this null and
+        | just use "https" in the host path(s) above and you should be fine.
+        |
+        */
 
         'sslVerification' => null,
 
-        /**
-         * Retries.
-         *
-         * By default, the client will retry n times, where n = number of nodes in
-         * your cluster. If you would like to disable retries, or change the number,
-         * you can do so here.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_set_retries
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | HTTP Retries
+        |--------------------------------------------------------------------------
+        |
+        | By default, the client will retry n times, where n = number of nodes in
+        | your cluster. If you would like to disable retries, or change the number,
+        | you can do so here.
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_set_retries
+        |
+        */
 
         'retries' => null,
 
-        /**
-         * The remainder of the configuration options can almost always be left
-         * as-is unless you have specific reasons to change them.  Refer to the
-         * appropriate sections in the Elasticsearch documentation for what each option
-         * does and what values it expects.
-         */
-
-        /**
-         * Sniff On Start.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Start Sniffing
+        |--------------------------------------------------------------------------
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html
+        |
+        */
 
         'sniffOnStart' => false,
 
-        /**
-         * HTTP Handler.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_configure_the_http_handler
-         * @see http://ringphp.readthedocs.org/en/latest/client_handlers.html
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | HTTP Handler
+        |--------------------------------------------------------------------------
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_configure_the_http_handler
+        | See http://ringphp.readthedocs.org/en/latest/client_handlers.html
+        |
+        */
 
         'httpHandler' => null,
 
-        /**
-         * Connection Pool.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_connection_pool
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_connection_pool.html
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Connection Pooling
+        |--------------------------------------------------------------------------
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_connection_pool
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_connection_pool.html
+        |
+        */
 
         'connectionPool' => null,
 
-        /**
-         * Connection Selector.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_connection_selector
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_selectors.html
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Connection Selection
+        |--------------------------------------------------------------------------
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_connection_selector
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_selectors.html
+        */
 
         'connectionSelector' => null,
 
-        /**
-         * Serializer.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_serializer
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_serializers.html
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Serialization
+        |--------------------------------------------------------------------------
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_the_serializer
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_serializers.html
+        |
+        */
 
         'serializer' => null,
 
-        /**
-         * Connection Factory.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_a_custom_connectionfactory
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Connection Factory
+        |--------------------------------------------------------------------------
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/2.0/_configuration.html#_setting_a_custom_connectionfactory
+        |
+        */
 
         'connectionFactory' => null,
 
-        /**
-         * Endpoint.
-         *
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/6.0/_configuration.html#_set_the_endpoint_closure
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | HTTP Endpoint
+        |--------------------------------------------------------------------------
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/6.0/_configuration.html#_set_the_endpoint_closure
+        |
+        */
 
         'endpoint' => null,
 
-        /**
-         * Register additional namespaces.
-         *
-         * An array of additional namespaces to register.
-         *
-         * @example 'namespaces' => [XPack::Security(), XPack::Watcher()]
-         * @see https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/ElasticsearchPHP_Endpoints.html#Elasticsearch_ClientBuilderregisterNamespace_registerNamespace
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Namespace Registration
+        |--------------------------------------------------------------------------
+        |
+        | An array of additional namespaces to register.
+        | For example: [XPack::Security(), XPack::Watcher()]
+        |
+        | See https://www.elastic.co/guide/en/elasticsearch/client/php-api/current/ElasticsearchPHP_Endpoints.html#Elasticsearch_ClientBuilderregisterNamespace_registerNamespace
+        |
+        */
 
         'namespaces' => [],
+
     ],
 
     /*
-     * Choose the method of indexing.
-     *
-     * SimpleIndexer indexes the data document by document.
-     * \Rennokki\ElasticScout\Indexers\SimpleIndexer::class
-     *
-     * MultipleIndexer indexes the data in bulks.
-     * \Rennokki\ElasticScout\Indexers\MultipleIndexer::class
-     */
+    |--------------------------------------------------------------------------
+    | Indexing Method
+    |--------------------------------------------------------------------------
+    |
+    | To index, you can either index one-by-one, or bulk-index multiple
+    | models at once, by chunking.
+    |
+    | Available classes:
+    | "\Rennokki\ElasticScout\Indexers\SimpleIndexer::class" - single index
+    | "\Rennokki\ElasticScout\Indexers\MultipleIndexer::class" - bulk index
+    |
+    */
 
     'indexer' => \Rennokki\ElasticScout\Indexers\SimpleIndexer::class,
 
     /*
-     * Each time a document is created, updated or deleted, update the mapping
-     * attached to the index of the model.
-     */
+    |--------------------------------------------------------------------------
+    | Sync Mapping on Save
+    |--------------------------------------------------------------------------
+    |
+    | Each time a document is created, updated or deleted, update the mapping
+    | attached to the index of the model. This can be used as alternative
+    | to the sync index command.
+    |
+    */
 
     'sync_mapping_on_save' => env('SCOUT_ELASTICSEARCH_SYNC_MAPPING_ON_SAVE', true),
 
     /*
-     * Elasticsearch "caches" some of the documents and reveals them
-     * when it thinks it's the best to. Enabling it will lead to a lot of
-     * refreshes that can slow the performance on production.
-     *
-     * Recommended for production: false
-     * Recommended for local/testing: true
-     */
+    |--------------------------------------------------------------------------
+    | Refresh Documents on Save
+    |--------------------------------------------------------------------------
+    |
+    | Elasticsearch "caches" some of the documents and reveals them
+    | when it thinks it's the best to. Enabling it will lead to a lot of
+    | refreshes that can slow the performance. Recommended to be set "false"
+    | if the documents are not needed to be available as soon
+    | as they get updated.
+    |
+    */
 
     'refresh_document_on_save' => env('SCOUT_ELASTICSEARCH_REFRESH_DOCUMENT_ON_SAVE', true),
+
 ];
